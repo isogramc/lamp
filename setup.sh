88bc26baa41fd7dec24dcf0bed48156b8fa20528
isogramc <theresa@isogramconsulting.com>
2019-08-19 13:23:33 +0200

    scripts

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 80:80 -p 3306:3306 -v /Code/lamp:/app --name="isolamp" tutum/lamp
+docker run -d -p 80:80 -p 3306:3306 -e MYSQL_PASS="asdf" -v /Code/lamp:/app --name="isolamp" tutum/lamp
